f6cb427f7751f0099adb18c28d82be7ca4921478
dbera17 <61909438+dbera17@users.noreply.github.com>
2023-05-09 14:50:20 +0400

    added reset pass markup

---
+++ client/pages/forget-password.js
@@ -0,0 +1,47 @@
+import { Layout as AntLayout, Button, Input, Form, Row, Col, Typography } from 'antd';
+import Link from 'next/link';
+import { useState } from 'react';
+import Layout from '@/components/Layout';
+
+
+function ForgetPassword({ }) {
+  const [isSending, setIsSending] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  function onFormFinish(values) {
+    setIsSending(true);
+  }
+  return (
+    <Layout>
+      <AntLayout.Content style={{ paddingBottom: 36 }}>
+        <Row style={{ height: '100%' }} align="middle">
+          <Col span={24} md={12}>
+            <Form layout="vertical" onFinish={onFormFinish} style={{ maxWidth: 380, margin: '0 auto' }} requiredMark={false}>
+              <Form.Item><Typography.Title level={2}>პაროლის აღდგენა</Typography.Title></Form.Item>
+              {isSuccess ?
+                <Form.Item><Typography.Title level={5}>პაროლის აღდგენის ინსტრუქცია გაგზავნილია თქვენს ელ-ფოსტაზე</Typography.Title></Form.Item>
+                :
+                <>
+                  <Form.Item label="ელ-ფოსტა" name="email" rules={[{ required: true, message: 'გთხოვთ შეიყვანოთ ელ-ფოსტა' }]}>
+                    <Input size="large" />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button size="large" loading={isSending} type="primary" htmlType="submit" className="dark-button" block>გაგზავნა</Button>
+                  </Form.Item>
+                </>}
+              <Form.Item>
+                <Link href="/login">
+                  <Button style={{ padding: 0, fontWeight: 'bold' }} type="link">ავტორიზაცია</Button>
+                </Link>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col span={0} md={12}>
+            <div className="side-image reset-image"></div>
+          </Col>
+        </Row>
+      </AntLayout.Content>
+    </Layout>
+  )
+}
+
+export default ForgetPassword;
