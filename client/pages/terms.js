348fac012cdbcf6f313e5f5cf463a8cf114d0274
dbera17 <61909438+dbera17@users.noreply.github.com>
2023-05-12 20:31:41 +0400

    Need images copyrighter to terms and conditions

--- client/pages/terms.js
+++ client/pages/terms.js
@@ -1,32 +1,50 @@
-import { Layout as AntLayout, Button, Input, Form, Row, Col, Typography, Space } from 'antd';
-import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { Layout as AntLayout, Row, Col, Typography, List } from 'antd';
 import Layout from '../components/Layout';
 
+const { Title, Paragraph } = Typography;
 
-function Register({ }) {
-  const [isRegistering, setIsRegistering] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
-  useEffect(() => {
-    if (localStorage.getItem('jwt')) {
-      Router.replace('/me');
-    }
-  }, []);
-  function onFormFinish(values) {
-    setIsRegistering(true);
-    values.username = values.email; 
-    console.log(valies);
-  }
-
+function Terms() {
   return (
     <Layout>
-      <AntLayout.Content style={{ paddingBottom: 36 }}>
-        <Row style={{ height: '100%' }} align="middle">
+      <AntLayout.Content style={{ padding: '0 50px' }}>
+        <Row style={{ marginTop: 64 }} justify="center">
           <Col span={24} md={12}>
-            TODO TERMS
+            <Title level={1}>Terms and Conditions</Title>
+            <Paragraph>
+              Welcome to DevDesHub! These terms and conditions outline the rules and regulations for the use of DevDesHub's Website. By accessing this website we assume you accept these terms and conditions in full. Do not continue to use DevDesHub's website if you do not accept all of the terms and conditions stated on this page.
+            </Paragraph>
+            <Title level={2}>License</Title>
+            <Paragraph>
+              Unless otherwise stated, DevDesHub and/or its licensors own the intellectual property rights for all material on DevDesHub. All intellectual property rights are reserved. You may view and/or print pages from https://www.devdeshub.com for your own personal use subject to restrictions set in these terms and conditions.
+            </Paragraph>
+            <List
+              header={<Title level={3}>You must not:</Title>}
+              bordered
+              dataSource={[
+                'Republish material from DevDesHub',
+                'Sell, rent or sub-license material from DevDesHub',
+                'Reproduce, duplicate or copy material from DevDesHub'
+              ]}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+            <Title level={2}>Hyperlinking to our Content</Title>
+            <Paragraph>
+              Organizations may link to our home page, to publications or to other Web site information so long as the link:.
+            </Paragraph>
+            <List
+              header={<Title level={3}>You must not:</Title>}
+              bordered
+              dataSource={[
+                'Is not in any way misleading',
+                'Does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services',
+                'Fits within the context of the linking partys site'
+              ]}
+              renderItem={item => <List.Item>{item}</List.Item>}
+            />
+            {/* Continue with your terms and conditions... */}
           </Col>
-          <Col span={0} md={12}>
-            SAME HERE
+          <Col span={24} md={12}>
+            {/* You might want to add additional information or images here */}
           </Col>
         </Row>
       </AntLayout.Content>
@@ -34,4 +52,4 @@ function Register({ }) {
   )
 }
 
-export default Register;
+export default Terms;
